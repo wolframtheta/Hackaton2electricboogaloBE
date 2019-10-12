3802298e3582765a8de1f355156458a47c70e070
Xavier Marques <xaviermarques4f@gmail.com>
2019-10-12 18:04:19 +0200

    Can create users in mongodb

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var fs = require('fs');
 const mongoose = require('mongoose');
-
-const urlDB = process.env.DB || 'mongodb+srv://ubersaw:ubersaw123@cluster-naoru.mongodb.net/test?retryWrites=true&w=majority';
+const bcrypt = require('bcrypt');
+const saltRounds = 8;
+const urlDB = process.env.DB || 'mongodb+srv://ubersaw:ubersaw123@cluster-naoru.mongodb.net/anima_app?retryWrites=true&w=majority';
 app = express();
 port = process.env.PORT || 8888;
 
+
+
+app.post('/newUser', function(req, res) {
+
+ 
+  mongoose.connect(urlDB, {useNewUrlParser: true, useUnifiedTopology: true}, function(error) {
+    // if error is truthy, the initial connection failed.
+    bcrypt.hash(req.query.password, saltRounds, function(err, hash) {
+      if (error) console.log(error);
+      var User = mongoose.model('User', new mongoose.Schema({username: 'string', password: 'string' }));
+      var newUser = new User({username: req.query.username, password: hash });
+      newUser.save(function (err) {
+        if (err) return error;
+      });
+    });
+  });
+  
+});
 app.get('/login', function(req, res) {
   
   console.log('Login')
